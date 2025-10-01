66e3f1ae79ac15a424be2e436ad4a3cd2635b6ba
radekgersz <radekgersz@gmail.com>
2025-10-01 13:29:44 +0000

    updating cirpts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,7 +4,7 @@ DATE="20250920"      # change to latest dump date
 WIKI="plwiki"
 DUMP_URL="https://dumps.wikimedia.org/$WIKI/$DATE"
 DB_NAME="wikipedia"
-MYSQL="mysql -u root -p$MYSQL_PWD $DB_NAME
+MYSQL="sudo mysql -u root $DB_NAME"
 
 mkdir -p dumps
 cd dumps
@@ -15,7 +15,7 @@ wget -c https://dumps.wikimedia.org/plwiki/latest/plwiki-latest-page.sql.gz
 
 cd .. 
 
-mysql -u root -p$MYSQL_PWD -e "DROP DATABASE IF EXISTS $DB_NAME; CREATE DATABASE $DB_NAME;"
+sudo mysql -u root -e "DROP DATABASE IF EXISTS $DB_NAME; CREATE DATABASE $DB_NAME;"
 
 zcat dumps/plwiki-latest-page.sql.gz       | $MYSQL
 zcat dumps/plwiki-latest-pagelinks.sql.gz | $MYSQL
