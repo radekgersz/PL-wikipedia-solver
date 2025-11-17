fd0a5239d15d9850ee0fdae3e8cd1d9efab6d269
radekgersz <radekgersz@gmail.com>
2025-11-17 14:06:16 +0100

    final tweaks

--- src/DatabaseHelpers.py
+++ src/DatabaseHelpers.py
@@ -1,16 +1,22 @@
 from huggingface_hub import hf_hub_download
 from sqlalchemy import create_engine
 from sqlalchemy.pool import StaticPool
+import os
 
-def downloadDatabase(repoID,accessToken,filename):
+def downloadDatabase(repoID, accessToken, filename):
+    CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+    PROJECT_ROOT = os.path.abspath(os.path.join(CURRENT_DIR, ".."))
+    dataset_dir = os.path.join(PROJECT_ROOT, "dataset")
+    os.makedirs(dataset_dir, exist_ok=True)
     dataset_path = hf_hub_download(
         repo_id=repoID,
         repo_type="dataset",
         token=accessToken,
-        local_dir='../dataset',
-        dry_run=False,
-        filename=filename
-        )
+        filename=filename,
+        local_dir=dataset_dir,
+        dry_run=False
+    )
+
     return dataset_path
 def createSQLiteEngine(databasePath):
     URI = f'sqlite:///{databasePath}'
